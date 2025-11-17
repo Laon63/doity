41a1b6243cbe7b61ea716d05cb00631a4b2b800d
song <thdtn63@hanmail.net>
2025-11-17 19:43:21 +0900

    [feat] - Init Doity prj

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
+import refreshPlugin from "eslint-plugin-react-refresh";
+import prettierConfig from "eslint-config-prettier";
+import js from "@eslint/js";
+
+export default [
+  {
+    ignores: ["dist/**", "eslint.config.js"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    plugins: {
+      react,
+      "react-hooks": hooksPlugin,
+      "react-refresh": refreshPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...hooksPlugin.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react-refresh/only-export-components": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  prettierConfig,
+];
