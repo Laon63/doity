46c3cebd11795cf2d6178eb56985f0c3ad086159
song <thdtn63@hanmail.net>
2025-11-22 21:27:51 +0900

    [temp] - supabase key 임시처리

--- src/client/lib/supabaseClient.ts
+++ src/client/lib/supabaseClient.ts
@@ -1,11 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase 프로젝트 URL 및 anon 키를 환경 변수에서 가져옵니다.
-const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL ||
-  'https://ylwjwnptevikhncmxuhw.supabase.co';
+const supabaseUrl = 'https://ylwjwnptevikhncmxuhw.supabase.co';
 const supabaseAnonKey =
-  import.meta.env.VITE_SUPABASE_ANON_KEY ||
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlsd2p3bnB0ZXZpa2huY214dWh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzOTEzMTUsImV4cCI6MjA3ODk2NzMxNX0.ETjWVJijGRdydUn4dNhXqmciW-GBGDBJKj0YhdbEw0U';
 
 // Supabase 클라이언트를 생성합니다.
